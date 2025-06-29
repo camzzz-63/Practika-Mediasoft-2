c3a529dde529bd8fb846901c5334346526caec4f
camzzz-63 <kamillazabbarova28@gmail.com>
2025-06-29 13:33:14 +0400

    feat: добавлен класс Car с полями VIN, модель, пробег, цена и тип CarType

---
+++ src/Car.java
@@ -0,0 +1,48 @@
+import java.util.Objects;
+
+public class Car implements Comparable<Car> {
+    private String vin;
+    private String model;
+    private String manufacturer;
+    private int year;
+    private int mileage;
+    private double price;
+
+    public Car(String vin, String model, String manufacturer, int year, int mileage, double price) {
+        this.vin = vin;
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.year = year;
+        this.mileage = mileage;
+        this.price = price;
+    }
+
+    public String getVin() { return vin; }
+    public String getModel() { return model; }
+    public String getManufacturer() { return manufacturer; }
+    public int getYear() { return year; }
+    public int getMileage() { return mileage; }
+    public double getPrice() { return price; }
+
+   @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Car car)) return false;
+        return vin.equals(car.vin);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(vin);
+    }
+
+    @Override
+    public int compareTo(Car other) {
+        return Integer.compare(other.year, this.year); // от новых к старым
+    }
+
+    @Override
+    public String toString() {
+        return vin + " - " + model + " (" + year + ")";
+    }
+}
